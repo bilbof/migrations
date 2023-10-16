6ce9a70ce2cb6991f5930da5c1c9c62156d42f3f
Bill Franklin <b.franklin@outlook.com>
2023-10-16 21:55:46 +0100

    Fixed about page links

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 const AboutPage = () => {
     document.title = "Migrations | About"
     const linkClasses = "text-blue-600 dark:text-blue-500 hover:underline"
-    const goToRust = "/?query=%7E%27*22go*22*2c*20*22rust*22"
-    const mongoToPostgres = "/?query=~%27mongo*2c*20postgres"
-    const herokoToKubernetes = "/?query=%7E%27*22heroku*22*2c*20kubernetes"
+    const groups = "groups=~%28~%27infrastructure~%27database~%27finance~%27library~%27mobile~%27place~%27programming~%27software~%27tool~%27writing%29"
+    const goToRust = `/?query=%7E%27*22go*22*2c*20*22rust*22&${groups}`
+    const mongoToPostgres = `/?query=~%27mongo*2c*20postgres&${groups}`
+    const herokoToKubernetes = `/?query=%7E%27*22heroku*22*2c*20kubernetes&${groups}`
     const WaveEmoji = (
         <span role="img" aria-label="wave">👋</span>
     )
@@ -21,10 +22,10 @@ const AboutPage = () => {
                 </a>), after I attended Richard's <a href="https://www.youtube.com/watch?v=mpY1lxkikqM" className={linkClasses}>talk</a> at DevopsDaysLondon about the GOV.UK infrastructure migration, which I had worked on.
             </p>
             <p className="mb-2 dark:text-white">
-                I thought it would be useful for future infrastructure migrations to have a dataset of previous migrations to learn from. The initial data was scraped from Hacker News, and accidentally included more migrations than intended (e.g. <Link to="/migrations/table?query=~%27*22seattle*22*2c*20*22california*22" className={linkClasses}>Seattle to California</Link>).
+                I thought it would be useful for future infrastructure migrations to have a dataset of previous migrations to learn from. The initial data was scraped from Hacker News, and accidentally included more migrations than intended (e.g. <Link to={`/table?query=~%27*22seattle*22*2c*20*22california*22&${groups}`} className={linkClasses}>Seattle to California</Link>).
             </p>
             <p className="mb-2 dark:text-white">
-                You can see how migrations change over time, filtered by segment, and custom search queries (e.g. <Link to={herokoToKubernetes} className={linkClasses}><pre style={{display: "inline"}}>"heroku", kubernetes</pre></Link>, will perform an exact match on Heroku with a fuzzy match on kubernetes). Some interesting patterns emerge (Rubyists moving to Node.JS and then Go), and some ideas for migrations, including <Link to="/migrations/?query=~%27*22reactjs*22*2c*20*22nextjs*22" className={linkClasses}>ReactJS to NextJS</Link> and <Link to="/migrations/?query=~%27*22netlify*22" className={linkClasses}>Google Kubernetes Engine to Netlify</Link>.
+                You can see how migrations change over time, filtered by segment, and custom search queries (e.g. <Link to={herokoToKubernetes} className={linkClasses}><pre style={{display: "inline"}}>"heroku", kubernetes</pre></Link>, will perform an exact match on Heroku with a fuzzy match on kubernetes). Some interesting patterns emerge (Rubyists moving to Node.JS and then Go), and some ideas for migrations, including <Link to={`/?query=~%27*22reactjs*22*2c*20*22nextjs*22&${groups}`} className={linkClasses}>ReactJS to NextJS</Link> and <Link to={`/?query=~%27*22netlify*22&${groups}`} className={linkClasses}>Google Kubernetes Engine to Netlify</Link>.
             </p>
             <p className="mb-2 dark:text-white">
                 The code is open source on GitHub, which you can find at <a href="https://github.com/bilbof/migrations" className={linkClasses}>bilbof/migrations</a>.
